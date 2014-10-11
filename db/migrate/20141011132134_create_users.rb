96db7c5270a0d13079b335bc852bc39aa64f116a
shehryarriaz <shehryarriaz@gmail.com>
2014-10-11 14:36:20 +0100

    Setting default role to User as basic_user.

--- db/migrate/20141011132134_create_users.rb
+++ db/migrate/20141011132134_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name
-      t.string :role
+      t.string :role, :default => "basic_user"
 
       t.timestamps
     end
