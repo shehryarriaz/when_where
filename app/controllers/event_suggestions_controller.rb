4ac175946a575eb649c45766d932c8778f000a7a
stefanielim <stefanie.lim04@gmail.com>
2014-10-13 20:58:04 +0100

    Adds views files for event suggestions and changes method to get hosting events in ES index page to be based on current user

--- app/controllers/event_suggestions_controller.rb
+++ app/controllers/event_suggestions_controller.rb
@@ -5,6 +5,8 @@ class EventSuggestionsController < ApplicationController
   # GET /event_suggestions
   # GET /event_suggestions.json
   def index
+    @events = EventSuggestion.all
+
     @events_as_host = current_user.events_as_host
     @events_as_invitee = current_user.events_as_invitee
     @event_choices = current_user.event_choices rescue nil
