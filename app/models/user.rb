626f8061e82e2e1766590965552006ebfa0de954
stefanielim <stefanie.lim04@gmail.com>
2014-10-13 12:33:01 +0100

    fixes association in the user model so that user.events_as_invitee works

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,12 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :role, :email, :password, :password_confirmation, :remember_me
+  
+  has_many :invitations, foreign_key: 'invitee_id'
+  has_many :events_as_invitee, through: :invitations, source: :event_suggestion
 
   has_many :events_as_host, foreign_key: 'host_id', class_name: 'EventSuggestion', dependent: :destroy
-  has_many :events_as_invitee, foreign_key: 'invitee_id', class_name: 'EventSuggestion'
+
   has_many :event_choices
   has_many :events, through: :event_choices
 end
