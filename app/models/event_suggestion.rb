826dbb72fd75a83228fd94bd3acff482c5c1e9e3
stefanielim <stefanie.lim04@gmail.com>
2014-10-14 15:53:52 +0100

    Adds a submit button to ES form and adds a callback function on ES create to create associated events for the date range given

--- app/models/event_suggestion.rb
+++ app/models/event_suggestion.rb
@@ -1,8 +1,27 @@
 class EventSuggestion < ActiveRecord::Base
-  attr_accessible :description, :end_date, :end_time, :location, :name, :start_date, :start_time, :status, :time_of_day
+  attr_accessible :name, :description, :location, :start_date, :end_date, :start_time, :end_time, :time_of_day, :status
 
   belongs_to :host, class_name: 'User'
   has_many :invitations
   has_many :invitees, through: :invitations
   has_many :events
+
+  validates :name, presence: :true
+  validates :location, presence: :true
+  validates :start_date, presence: :true
+
+  after_create :create_associated_events
+
+  private
+
+  def create_associated_events
+    if !end_date
+      Event.create(date: start_date, event_suggestion_id: id)
+    else
+      (start_date..end_date).each do |date|
+        Event.create(date: date, event_suggestion_id: id)
+      end
+    end
+  end
+
 end
