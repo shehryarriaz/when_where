36239c4ff5bc86364bfe9912a42459b51de51e24
shehryarriaz <shehryarriaz@gmail.com>
2014-10-15 23:01:46 +0100

    Adds finer authorization controls to ability file for EventSuggestion. Breaks choices however. Pending fix.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,11 +8,15 @@ class Ability
     elsif user.role? :basic_user
       can :read, User
       can :create, EventSuggestion
-      can :read, EventSuggestion
+      can :read, EventSuggestion do |event_suggestion|
+        Invitation.where(event_suggestion_id: event_suggestion.id, invitee_id: user.id).any?
+        end
+      can :read, EventSuggestion, host_id: user.id
       can :update, EventSuggestion, host_id: user.id
       can :destroy, EventSuggestion, host_id: user.id
       can :create, Invitation, host_id: user.id
       can :create, EventChoice
+      can :destroy, EventChoice
     else
       can :create, User
     end
