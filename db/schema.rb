fd7f14253917460093713c7e2b25b8399b412963
shehryarriaz <shehryarriaz@gmail.com>
2014-10-18 23:00:51 +0100

    Initial commit of Geocoder. Adds gem and migration to add latitude and longitude attributes to Venue model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141018201252) do
+ActiveRecord::Schema.define(:version => 20141018215910) do
 
   create_table "event_choices", :force => true do |t|
     t.integer  "event_id"
@@ -95,6 +95,8 @@ ActiveRecord::Schema.define(:version => 20141018201252) do
     t.string   "address"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
 end
