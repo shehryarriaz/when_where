aebeec006994fdfa28dbf8a1c4f0bea13fc96d11
shehryarriaz <shehryarriaz@gmail.com>
2014-10-18 23:06:51 +0100

    Adds Geocoder to Venue mode to save lat/long values.

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,8 +1,11 @@
 class Venue < ActiveRecord::Base
-  attr_accessible :address, :name
+  attr_accessible :address, :name, :latitude, :longitude
 
   has_many :event_venues
   has_many :event_suggestions, through: :event_venues
+
+  geocoded_by :address
+  after_validation :geocode
 
   validates :name, presence: :true
   validates :address, presence: :true
