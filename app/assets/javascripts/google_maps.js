fa4b4de5e7573b06cd4838e9e06eb692d13e6e25
shehryarriaz <shehryarriaz@gmail.com>
2014-10-19 14:41:49 +0100

    Adds set Google maps marker to map.

--- app/assets/javascripts/google_maps.js
+++ app/assets/javascripts/google_maps.js
@@ -1,16 +1,21 @@
 var myMap = myMap || {};
 
 myMap.initialize = function() {
-  console.log('Map working')
-  
-  var mapOptions = {
-      center: { lat:  51.52, lng: -0.115},     
-      zoom: 14,
-      mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
 
-    var map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
+  // MAP
+  var mapOptions = {
+    center: { lat:  51.52, lng: -0.115},     
+    zoom: 14,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  // MARKER
+  var markerOptions = {
+    position: new google.maps.LatLng(51.53, -0.109446)
+  }
+  var marker = new google.maps.Marker(markerOptions);
+  marker.setMap(map);
 }
 
 google.maps.event.addDomListener(window, 'load', myMap.initialize);
