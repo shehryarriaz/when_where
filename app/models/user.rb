f5e77481bbdf9c07eef5939733f48c936eca2a5e
stefanielim <stefanie.lim04@gmail.com>
2014-10-19 15:42:00 +0100

    Adds a method in the user model to handle the information received from google oauth to find or create user and adds a 'sign out' route to destroy user session

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,20 @@ class User < ActiveRecord::Base
   def role?(role_to_compare)
     self.role.to_s == role_to_compare.to_s
   end
+
+  def self.from_omniauth(auth)
+    if user = User.find_by_email(auth.info.email)
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user
+    else
+      where(auth.slice(:provider, :uid)).first_or_create do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.name = auth.info.name
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0,20]
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 WhenWhere::Application.routes.draw do
   root to: 'event_suggestions#index'
   devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
+  devise_scope :user do
+    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
   resources :users
   resources :event_venues
   resources :event_suggestions do
