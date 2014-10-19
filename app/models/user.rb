a26b6c9ca685ae13ecb305aa0e5a1d507babd6a4
stefanielim <stefanie.lim04@gmail.com>
2014-10-19 14:45:47 +0100

    Adds google oauth2 to gemfile, runs migration to add provider and uid to users db table and adds attr accessible fields in the user model for provider and uid

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, omniauth_providers: [:google_oauth2]
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :name, :role, :email, :password, :password_confirmation, :remember_me, :event_ids
+  attr_accessible :name, :role, :email, :password, :password_confirmation, :remember_me, :event_ids, :provider, :uid
   
   has_many :invitations, foreign_key: 'invitee_id'
   has_many :events_as_invitee, through: :invitations, source: :event_suggestion
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141018201252) do
+ActiveRecord::Schema.define(:version => 20141019134231) do
 
   create_table "event_choices", :force => true do |t|
     t.integer  "event_id"
@@ -85,6 +85,8 @@ ActiveRecord::Schema.define(:version => 20141018201252) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
