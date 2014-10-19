5eb5d50c28e4d8265c1ca4974f98fcadb108cd66
shehryarriaz <shehryarriaz@gmail.com>
2014-10-19 15:51:09 +0100

    Adds markers to Google maps drawing at Venue lat/long coordinates.

--- app/assets/javascripts/google_maps.js
+++ app/assets/javascripts/google_maps.js
@@ -3,6 +3,7 @@ var myMap = myMap || {};
 myMap.initialize = function() {
   var mapCanvas = $('#map-canvas')[0];
   var eventVenuePath = $(location).attr('pathname');
+  console.log(eventVenuePath);
 
   if (mapCanvas){
     // AJAX FOR COORDINATES
@@ -16,41 +17,49 @@ myMap.initialize = function() {
         dataType: 'JSON',
         success: function(data) {
           var venueId = data.venue_id;
+          console.log(venueId);
           $.ajax({
             url: '/venues/' + venueId,
             type: 'GET',
             dataType: 'JSON',
             success: function(data) {
-              var venueId = data.venue_id
-              console.log(venueId)
+              console.log(data);
+              setupMap(data);
             }
           });
         }
       });
     }
 
-    // MAP
-    var mapOptions = {
-      center: { lat:  51.52, lng: -0.115},     
-      zoom: 14,
-      mapTypeId:google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    function setupMap(data) {
+      var latitude = data.latitude
+      var longitude = data.longitude
 
-    // MARKER
-    var markerOptions = {
-      position: new google.maps.LatLng(51.53, -0.109446)
-    }
-    var marker = new google.maps.Marker(markerOptions);
-    marker.setMap(map);
+      console.log(latitude, longitude);
 
-    // INFO WINDOW
-    var infoWindowOptions = {
-      content: 'We Are Here!',
-      position: new google.maps.LatLng(51.535, -0.10945)
+      // MAP
+      var mapOptions = {
+        center: { lat: latitude, lng: longitude},
+        zoom: 14,
+        mapTypeId:google.maps.MapTypeId.ROADMAP
+      };
+      var map = new google.maps.Map( mapCanvas, mapOptions);
+
+      // MARKER
+      var markerOptions = {
+        position: new google.maps.LatLng( latitude, longitude)
+      }
+      var marker = new google.maps.Marker(markerOptions);
+      marker.setMap(map);
+
+      // INFO WINDOW
+      var infoWindowOptions = {
+        content: 'We Are Here!',
+        position: new google.maps.LatLng(51.535, -0.10945)
+      }
+      var popup = new google.maps.InfoWindow(infoWindowOptions);
     }
-    var popup = new google.maps.InfoWindow(infoWindowOptions);
-  }
+  }   
 }
 
 google.maps.event.addDomListener(window, 'load', myMap.initialize);
