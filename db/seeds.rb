762fc5e1fc823d9daa3244406ba0cad425aade7a
stefanielim <stefanie.lim04@gmail.com>
2014-10-20 10:23:14 +0100

    Changes db seed file to delete all existing data and create two admin users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 User.delete_all
+Event.delete_all
+EventChoice.delete_all
+EventSuggestion.delete_all
+EventVenue.delete_all
+Invitation.delete_all
+Venue.delete_all
 
-User.create(name: "Shehryar Riaz", role: "admin", email: "shehryar@ga.com", password: "password", password_confirmation: "password")
-User.create(name: "Stefanie Lim", role: "admin", email: "stef@ga.com", password: "password", password_confirmation: "password")
-User.create(name: "Kate Montgomery", role: "basic_user", email: "kate@ga.com", password: "password", password_confirmation: "password")
-User.create(name: "Laurence James", role: "basic_user", email: "laurence@ga.com", password: "password", password_confirmation: "password")
-User.create(name: "Francesco Papini", role: "basic_user", email: "francesco@ga.com", password: "password", password_confirmation: "password")
+User.create(name: "Shehryar Riaz", role: "admin", email: "shehryarriaz@gmail.com", password: "Dinner0rdrink$", password_confirmation: "Dinner0rdrink$")
+User.create(name: "Stefanie Lim", role: "admin", email: "stefanie.lim04@gmail.com", password: "Dinner0rdrink$", password_confirmation: "Dinner0rdrink$")
